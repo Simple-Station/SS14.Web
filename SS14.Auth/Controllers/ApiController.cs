6aa04c7c1f7b0d4c0cea9d7b0bb1eab965ec87f1
DEATHB4DEFEAT <zachcaffee@outlook.com>
2025-01-29 20:28:48 -0800

    add auth's relevant account site to api

---
+++ SS14.Auth/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SS14.Auth.Controllers;
+
+[ApiController]
+[Route("/api")]
+public sealed class ApiController(IConfiguration cfg) : ControllerBase
+{
+    private string WebBaseUrl => cfg.GetValue<string>("WebBaseUrl") ?? "";
+
+    [HttpGet("accountSite")]
+    public Task<IActionResult> GetAccountSite()
+    {
+        return Task.FromResult<IActionResult>(Ok(new {WebBaseUrl}));
+    }
+}
